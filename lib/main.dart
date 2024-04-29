b3a43a2d39909762f41f0662244d8e7d59848ed3
Sahil <rk4852115@gmail.com>
2024-04-29 15:32:33 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const Button(),
+      },
+    );
+  }
+}
+
+class Button extends StatefulWidget {
+  const Button({super.key});
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.black, width: 1),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: BorderDirectional(bottom: BorderSide(width: 1,)),
+                  ),
+                  child: Text('Common Button')),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Elevated'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text('Outlined'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Text Button'),
+              ),
+              FilledButton(
+                onPressed: () {},
+                child: const Text('Filled Button'),
+              ),
+              FilledButton.tonal(
+                onPressed: () {},
+                child: const Text('Filled Tonal'),
+              ),
+              FloatingActionButton(onPressed: () {
+
+              },child: const Icon(Icons.add),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
